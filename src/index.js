d6307974a597c06fdf2e3f2e358f5d4cbaeac680
Alexandru Ghiur <ghalex@gmail.com>
2017-07-09 23:51:41 +0300

    Add darken & lighten

--- src/index.js
+++ src/index.js
@@ -25,3 +25,30 @@ export const fromProps = (props) => {
 
   return props.palette || 'black'
 }
+
+export const lum = (hex, value) => {
+  hex = String(hex).replace(/[^0-9a-f]/gi, '')
+  if (hex.length < 6) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]
+  }
+  value = (value || 0)
+
+  var rgb = '#'
+  var c
+  var i
+
+  for (i = 0; i < 3; i++) {
+    c = parseInt(hex.substr(i * 2, 2), 16)
+    c = Math.round(Math.min(Math.max(0, c + c * value), 255)).toString(16)
+    rgb += ('00' + c).substr(c.length)
+  }
+  return rgb
+}
+
+export const darken = (f, value) => {
+  return lum(f(), -value)
+}
+
+export const lighten = (f, value) => {
+  return lum(f(), value)
+}
